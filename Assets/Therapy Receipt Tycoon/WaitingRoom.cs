465d004cca4f3ef75915780f426652aa6b6809bd
Philipp <Philipp_Goecke@web.de>
2013-09-29 16:19:11 +0200

    -added navMeshAgent to Client -waiting Room; waiting Clients have a seat assigned

--- Assets/Therapy Receipt Tycoon/WaitingRoom.cs
+++ Assets/Therapy Receipt Tycoon/WaitingRoom.cs
@@ -10,20 +10,40 @@ public class WaitingRoom : MonoBehaviour {
 	
 	public event ClientEventHandler ClientEnteredWaitingRoom;
 	
-	private Queue<Client> waitingRoom = new Queue<Client>();
-	
 	public uint availableSeats = 6;
 	
+	private Queue<Client> waitingRoom = new Queue<Client>();
+	private Client[] orderedClients = new Client[6];	
+	public Transform[] SeatPosition = new Transform[6];
+
 	
 	public void SendClientToWaitingRoom(Client targetClient){
 		this.waitingRoom.Enqueue(targetClient);
+		for (int i = 0; i < availableSeats; i++)
+		{
+			if (orderedClients[i] == null)
+			{
+				targetClient.GetComponent<NavMeshAgent>().destination = SeatPosition[i].position;
+				orderedClients[i] = targetClient;
+				break;
+			}
+		}
 		Debug.Log(this.waitingRoom.Count + " people waiting in the waiting room");
 		
 		OnClientEnteredWaitingRoom(targetClient);
 	}
 	
 	public Client TakeClient(){
-		return this.waitingRoom.Dequeue();
+		Client NextClient = this.waitingRoom.Dequeue();
+		for (int i = 0; i < availableSeats; i++)
+		{
+			if 	(NextClient == orderedClients[i])
+			{
+				orderedClients[i] = null;
+				break;
+			}
+		}
+		return NextClient;
 	}
 	
 	public void OnClientEnteredWaitingRoom(Client currentClient){
